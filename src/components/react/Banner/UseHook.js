1a4440dd544ee5ad660c1dde21561e34a740bfc8
chiming <515072052@qq.com>
2022-01-27 11:15:58 +0800

    [feat] 修改hook笔记

--- src/components/react/Banner/UseHook.js
+++ src/components/react/Banner/UseHook.js
@@ -10,8 +10,16 @@ export default function UseHook() {
                             {`
         自定义Hook
         function useFriendStatus(friendID) {
-            const [isOnline, setIsOnline] = useState(null);
-            // ...
+            const [isOnline, setIsOnline] = useState(friendID||null);
+            useEffect(() => {
+              console.log("isOnline");
+              // ...
+              const Timer = setTimeout(() => {
+                // todo
+              }, 100);
+              // 若要在Didmount生命周期释放内存要 加 return
+              return ()=> clearSetTimeout(Timer)
+            }, []);
             return isOnline;
           }
         
@@ -29,7 +37,7 @@ export default function UseHook() {
           
             return (
               <li style={{ color: isOnline ? 'green' : 'black' }}>
-                {props.friend.name}
+                {useFriendStatus()}
               </li>
             );
         }
@@ -38,6 +46,7 @@ export default function UseHook() {
                     </pre>
                     <div>自定义 Hook 必须以 “use” 开头,在两个组件中使用相同的 Hook 不会共享 state。
                         因为在一个组件中多次调用 useState 和 useEffect，它们是完全独立的。
+
                     </div>
                     <p>
                         自定义 Hook 定义一个常量，方便我们调用变量，就像我们经常封装函数一样，可以直接调用
