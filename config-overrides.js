5bf53b913a312ee23d24ea9aeca82198fdff3b99
miss-tree <515072052@qq.com>
2022-01-27 17:26:50 +0800

    [feat] 配置装饰器

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, fixBabelImports, disableEsLint, addWebpackPlugin, addWebpackAlias} = require('customize-cra');
+const { override, fixBabelImports, disableEsLint, addWebpackPlugin, addWebpackAlias,addDecoratorsLegacy} = require('customize-cra');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
@@ -20,29 +20,30 @@ module.exports = override(
         libraryDirectory: 'es',
         style: 'css',
      }),
-     disableEsLint(),
-     addWebpackPlugin(
-         // 配置compression-webpack-plugin压缩
+    disableEsLint(),
+    addDecoratorsLegacy(), //配置装饰器
+    addWebpackPlugin(
+       // 配置compression-webpack-plugin压缩
         new CompressionWebpackPlugin({
-            //   filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
-              filename: '[path][base].gz', 
-              algorithm: 'gzip',
-              test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-              threshold: 1024*10,
-              minRatio: 0.8
-            }), 
-        new BundleAnalyzerPlugin({
-            analyzerMode: "server",
-            analyzerHost: "127.0.0.1",
-            analyzerPort: 8724, // 运行后的端口号
-        }),
-        new ProgressBarPlugin({
-            complete: "green",
-            format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
-            clear: true
-        })
+          //   filename: '[path].gz[query]', // 目标资源名称。[file] 会被替换成原资源。[path] 会被替换成原资源路径，[query] 替换成原查询字符串
+            filename: '[path][base].gz', 
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            threshold: 1024*10,
+            minRatio: 0.8
+          }), 
+      new BundleAnalyzerPlugin({
+          analyzerMode: "server",
+          analyzerHost: "127.0.0.1",
+          analyzerPort: 8724, // 运行后的端口号
+      }),
+      new ProgressBarPlugin({
+          complete: "green",
+          format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
+          clear: true
+      })
 
-         
-     )
+        
+    )
 )
 
